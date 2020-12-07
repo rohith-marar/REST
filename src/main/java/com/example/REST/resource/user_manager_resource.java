b10ca2713d0065ee15baa116b884f57b0852a2d4
rohith-marar <75575738+rohith-marar@users.noreply.github.com>
2020-12-07 18:58:00 +0530

    Add files via upload

---
+++ src/main/java/com/example/REST/resource/user_manager_resource.java
@@ -0,0 +1,21 @@
+package com.example.REST.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.REST.repository.user_manager_repository;
+
+
+@RestController
+public class user_manager_resource {
+	
+	@Autowired
+	user_manager_repository u_m_r;
+	
+	@RequestMapping(value="/user_manager")
+	public List<user_manager> getAll(){
+		return u_m_r.findAll();
+		
+	}
+
+}
